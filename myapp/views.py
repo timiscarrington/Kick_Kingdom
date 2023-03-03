366b4e32efe354a7d7e3437929235a7011ee3edb
TimisCarrington <timiscarrington@gmail.com>
2023-03-02 17:55:01 -0800

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def index(request): 
+    products= Product.objects.all()
+    return render(request, 'myapp/index.html',{'products':products})
